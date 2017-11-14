aed8276a8ff6fb0b8338a3835eab200f947c112e
James Davis <jamesmdavis95@gmail.com>
2017-11-13 22:24:48 -0800

    Health Manager
    Major Components:
    - AbstractHealthManager
    - HealthManager

---
+++ Seize Command/Assets/Scripts/Abstract/AbstractHealthManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractHealthManager : MonoBehaviour
+{
+    [SerializeField] float health;
+
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+        protected set
+        {
+            health = value;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.CompareTag("Projectile"))
+        {
+            PhysicalProjectile physProj = coll.gameObject.GetComponent<PhysicalProjectile>();
+            TakeDamage(physProj.ProjectileDamage);
+            Destroy(coll.gameObject);
+        }
+    }
+
+    protected virtual void TakeDamage(float damage)
+    {
+        Health -= damage;
+        CheckHealth();
+    }
+
+    void CheckHealth()
+    {
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
