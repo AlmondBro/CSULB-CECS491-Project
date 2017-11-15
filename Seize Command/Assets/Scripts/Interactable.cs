74845c4df6fd1d1dc470e20f02a905604e6e3364
Helen Pabst <hlnpabst@gmail.com>
2017-11-15 07:25:53 -0800

    Made Seat Interactable

---
+++ Seize Command/Assets/Scripts/Interactable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour, IInteractable
+{
+
+    //IInteractable interface
+    public void Interact()
+    {
+        //Do something
+        Debug.Log("call interact");
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
