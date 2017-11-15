157651092202bbc556b4931242bd06011298c2d9
James Davis <Jamesmdavis95@gmail.com>
2017-11-15 12:54:47 -0800

    Week One Demo
    Major Changes:
    - Fixed interaction System
    - Added Andrews Movement Component

--- Seize Command/Assets/Scripts/Low Level Functionality Scripts/InteractableScripts/Seat.cs
+++ Seize Command/Assets/Scripts/Low Level Functionality Scripts/InteractableScripts/Seat.cs
@@ -2,25 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour, IInteractable
+public class Seat : MonoBehaviour, IInteractable
 {
-
     //IInteractable interface
     public void Interact()
     {
         //Do something
         Debug.Log("call interact");
     }
-
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
 }
---
+++ Seize Command/Assets/Scripts/Low Level Functionality Scripts/PlayerScripts/PlayerInteractionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionManager : MonoBehaviour
+{
+    IInteractable interactable;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(interactable != null)
+            {
+                interactable.Interact();
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.CompareTag("Interactable"))
+        {
+            interactable = coll.gameObject.GetComponent<IInteractable>();
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if(coll.gameObject.CompareTag("Interactable"))
+        {
+            interactable = null;
+        }
+    }   
+}
