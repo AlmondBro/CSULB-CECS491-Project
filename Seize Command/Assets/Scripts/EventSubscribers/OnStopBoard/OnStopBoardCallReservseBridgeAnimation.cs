95597420299e6268e772169136f3415f63db8ffb
James Davis <jamesmdavis95@gmail.com>
2018-02-10 20:35:26 -0800

    Boarding System Update
    - Completed implementation of the boarding system

---
+++ Seize Command/Assets/Scripts/EventSubscribers/OnStopBoard/OnStopBoardCallReservseBridgeAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStopBoardCallReservseBridgeAnimation : AbstractSubscribers<ShipInteractable>
+{
+    void OnEnable()
+    {
+        type.onStopBoard += CallReverseBridgeAnimation;
+    }
+
+    void OnDisable()
+    {
+        if (type)
+        {
+            type.onStopBoard -= CallReverseBridgeAnimation;
+        }
+    }
+
+    void CallReverseBridgeAnimation(GameObject ship)
+    {
+        ship.GetComponent<Animator>().SetTrigger("ReverseBridge");
+    }
+}
