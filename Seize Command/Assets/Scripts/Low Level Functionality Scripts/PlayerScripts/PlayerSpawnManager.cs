c249cc1edd76f1c2347a51e143c0835c49096028
Alex Tol <atol514@gmail.com>
2018-02-24 14:22:26 -0800

    Networking patch 1
    Player movement networked and inependent. Player Camera movement independent (non-networked).

---
+++ Seize Command/Assets/Scripts/Low Level Functionality Scripts/PlayerScripts/PlayerSpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSpawnManager : NetworkBehaviour {
+
+	void Start()
+	{
+		if (!isLocalPlayer) {
+			return;
+		}
+		this.setPlayer (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	 void setPlayer()
+	{
+		Camera.main.GetComponent<CameraController> ().lockToPlayer (gameObject);
+	}
+}
