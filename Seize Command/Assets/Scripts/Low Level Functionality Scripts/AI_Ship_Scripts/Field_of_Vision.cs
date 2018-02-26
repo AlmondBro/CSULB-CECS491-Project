d0553d7e3d0cd93803d91465feeb21f04034ce30
Andrew Fung <andrewfungli@gmail.com>
2018-02-26 12:31:50 -0800

    created ai scene and started field of vision

---
+++ Seize Command/Assets/Scripts/Low Level Functionality Scripts/AI_Ship_Scripts/Field_of_Vision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field_of_Vision : MonoBehaviour {
+
+    public GameObject target;
+
+    public Field_of_Vision()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Target In Bound");
+            target = collision.gameObject;
+        }
+
+    }
+
+    public GameObject addNewTarget()
+    {
+        return target;
+    }
+
+}
--- Seize Command/Assets/Scripts/Low Level Functionality Scripts/PlayerScripts/PositionOffsetWhileSeated.cs
+++ Seize Command/Assets/Scripts/Low Level Functionality Scripts/PlayerScripts/PositionOffsetWhileSeated.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class PositionOffsetWhileSeated : MonoBehaviour
 {
     Rigidbody2D rb2dPlayer;
-    Rigidbody2D rb2dShip;
+    //Rigidbody2D rb2dShip;
     
 	void Start ()
     {
         Rigidbody2D[] rb2dArray = GetComponentsInParent<Rigidbody2D>();
         rb2dPlayer = rb2dArray[0];
-        rb2dShip = rb2dArray[1];
+        //rb2dShip = rb2dArray[1];
 	}
 	
 	void FixedUpdate ()
@@ -23,6 +23,6 @@ public class PositionOffsetWhileSeated : MonoBehaviour
     void OffSetPlayerPosition()
     {
         Vector2 playerVelocity = GetComponent<PlayerMovementManager>().MovementDirection;
-        rb2dPlayer.velocity = playerVelocity + rb2dShip.velocity;
+        rb2dPlayer.velocity = playerVelocity; //+ rb2dShip.velocity;
     }
 }
