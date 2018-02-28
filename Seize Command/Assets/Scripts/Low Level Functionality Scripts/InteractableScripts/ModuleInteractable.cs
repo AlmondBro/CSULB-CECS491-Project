1b4e107dc846dd7e1ba30b9db6bf5d67e486f9f2
James Davis <Jamesmdavis95@gmail.com>
2018-02-27 22:23:09 -0800

    Added the Module System
    - Added the Engine and Shield Modules
    - Added scripts to define module health and functionality
    - Added repair stations for each module
    - Created an Interface called IRepairable to define the ability to repair a given module.  The System uses this as an abstract reference to any module that can be repaired, allowing for extensibility if more modules were needed to be added

---
+++ Seize Command/Assets/Scripts/Low Level Functionality Scripts/InteractableScripts/ModuleInteractable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleInteractable : MonoBehaviour, IInteractable
+{
+    IRepairable repairSatation;
+
+    void Start()
+    {
+        repairSatation = GetComponentInChildren<IRepairable>();
+    }
+
+    public void Interact(GameObject interactor)
+    {
+        if(interactor.CompareTag("Character"))
+        {
+            repairSatation.Repair();
+        }
+    }
+}
