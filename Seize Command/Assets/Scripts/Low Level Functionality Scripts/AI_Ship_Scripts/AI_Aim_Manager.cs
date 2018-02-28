e508588f6fa256f1b654c929708c60fb39c86b20
Andrew Fung <andrewfungli@gmail.com>
2018-02-27 16:46:21 -0800

    Ship field of vision

---
+++ Seize Command/Assets/Scripts/Low Level Functionality Scripts/AI_Ship_Scripts/AI_Aim_Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Aim_Manager : MonoBehaviour{
+    private float rot_speed = 30;
+    AI_Ship_FieldofView fov;
+    protected GameObject target;
+
+    void Start()
+    {
+        fov = gameObject.AddComponent(typeof(AI_Ship_FieldofView)) as AI_Ship_FieldofView;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Aim();
+	}
+
+    void Aim()
+    {
+        target = fov.GetTarget();
+        if(target != null)
+        {
+            Debug.Log(target);
+            Vector3 distance = target.transform.position - transform.position;
+            distance.Normalize();
+            float rotation_z = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0f, 0f, rotation_z + -90);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, rot_speed * Time.deltaTime);
+        }
+    }
+}
