906fe7c1e2510918a14d54abe5db084620e3e629
Huy Le <huyle9613@gmail.com>
2018-03-21 10:10:13 -0700

    inputfield

---
+++ Main Menu/Assets/Unity UI Sample/Scripts/InputToString.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputToString : MonoBehaviour {
+
+    public string playerName;
+    public InputField input;
+    public Text displayText;
+
+    public void OnString_ToName()
+    {
+        playerName = input.text;
+        displayText.text = playerName;
+        Debug.Log("playerName variable now holds: " + playerName);
+    }
+}
